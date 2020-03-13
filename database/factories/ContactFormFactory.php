f8cabc6f3375d383b106d5d443842db963ae401b
Suhaib-ANE <suhaibsh612@gmail.com>
2020-03-13 19:14:51 +0200

    1

---
+++ database/factories/ContactFormFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ContactForm;
+use Faker\Generator as Faker;
+
+$factory->define(ContactForm::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
